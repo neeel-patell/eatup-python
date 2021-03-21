5581197acbc57bcbc29da5de2510d7f2ca1690b4
Neel Patel <neelpatel1905.np@gmail.com>
2021-03-21 12:59:34 +0530

    feat: product category web module

--- web/urls.py
+++ web/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path("", home, name="home"),
     path("recipe/category/", recipe_category, name="recipe_category"),
     path("recipe/category/<int:id>/", recipe_category_single, name="recipe_category_single"),
+    path("product/category/", view_product_category, name="view_product_category"),
+    path("product/category/add", add_product_category, name="add_product_category"),
+    path("product/category/<int:id>/delete", delete_product_category, name="delete_product_category"),
+    path("product/category/<int:id>/update", update_product_category, name="update_product_category"),
+    path("product/category/<int:id>", view_product_category_single, name="view_product_category_single"),
 ]
--- web/views.py
+++ web/views.py
@@ -7,12 +7,12 @@ import os
 def home(request):
     return render(request, "home.html")
 
-''' Recipe functions '''
+''' Recipe Category functions start '''
 
 def recipe_category(request):
     if request.method == "GET":
         categories = RecipeCategory.objects.order_by('name')
-        return render(request, "recipe.html", {'categories':categories})
+        return render(request, "recipe_category.html", {'categories':categories})
     elif request.method == "POST":
         name = request.POST['name']
         category = RecipeCategory(name=name)
@@ -51,3 +51,60 @@ def recipe_category_single(request, id):
             RecipeCategory.objects.filter(id=id).update(name=name)
         return redirect(reverse('recipe_category_single', kwargs={'id': id}))
 
+''' Recipe Category functions finish '''
+
+
+
+''' Product Category functions start '''
+
+def upload_product_image(f, id):  
+    with open("media/images/product_category/"+str(id)+".jpg", 'wb+') as destination:  
+        for chunk in f.chunks():  
+            destination.write(chunk)
+
+def add_product_category(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        category = ProductCategory(name=name)
+        category.save()
+        # id = ProductCategory.objects.latest('id')
+        # image = request.FILES['image']
+        # upload_product_image(image, id.id)
+        return redirect(reverse('view_product_category'))
+
+def view_product_category(request):
+    if request.method == "GET":
+        categories = ProductCategory.objects.order_by('name')
+        return render(request, "product_category.html", {'categories':categories})
+
+def delete_product_category(request, id):
+    if request.method == "GET":
+        category = ProductCategory.objects.get(pk=id)
+        category.delete()
+        # os.remove("media/images/product_category/"+str(id)+".jpg")
+        return redirect(reverse('view_product_category'))
+
+def update_product_category(request, id):
+    if request.method == "POST":
+        type = request.POST['type']
+        # if type == "image_change":
+        #     image = request.FILES['image']
+        #     if os.path.exists("media/images/product_category/"+str(id)+".jpg"):
+        #         os.remove("media/images/product_category/"+str(id)+".jpg")
+        #     upload_product_image(image, id)
+        if type == "name_change":
+            name = request.POST['name']
+            ProductCategory.objects.filter(id=id).update(name=name)
+        return redirect(reverse('view_product_category_single', kwargs={'id': id}))
+
+def view_product_category_single(request, id):
+    if request.method == "GET":
+        category = ProductCategory.objects.get(pk=id)
+        return render(request, "product_category_single.html", {'category':category})
+
+''' Product Category functions finish '''
+
+
+''' Product functions start '''
+
+''' Product functions finish '''
