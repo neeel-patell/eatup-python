5d5599e581b9fc89c2fddacd86fbf703010c9af0
Neel Patel <neelpatel1905.np@gmail.com>
2021-04-04 15:57:27 +0530

    feature: recipe by category api

--- api/urls.py
+++ api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('user/login/mobile', login_user_via_mobile, name="login_user_via_mobile"),
 
     path('recipe/category/all', get_all_recipe_category, name='get_all_recipe_category'),
+    path('recipe/<int:category>', get_recipe_by_category, name='get_recipe_by_category'),
 ]
--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from user.models import *
 import json
 from hashlib import sha256
 from django.http import JsonResponse
+import datetime
 
 
 def register_user(request):
@@ -15,13 +16,13 @@ def register_user(request):
         user = User(mobile=mobile, email=email, password=password)
         response = {}
         if User.objects.filter(email=email).exists():
-            response = {'status':1}
+            response = {'status':'1'}
         elif User.objects.filter(mobile=mobile).exists():
-            response = {'status':2}
+            response = {'status':'2'}
         else:
             user.save()
-            response = {'status':0}
-        return HttpResponse(json.dumps(response))
+            response = {'status':'0'}
+        return JsonResponse(response, safe=False)
 
 def login_user_via_email(request):
     if request.method == 'POST':
@@ -44,8 +45,27 @@ def login_user_via_mobile(request):
         return JsonResponse(response, safe=False)
 
 
-
 def get_all_recipe_category(request):
     if request.method == "GET":
         category = list(RecipeCategory.objects.values().order_by('name'))
         return JsonResponse({'data':category}, safe=False)
+
+def get_recipe_by_category(request, category):
+    if request.method == "GET":
+        queryset = Recipe.objects.filter(category_id=category).order_by('created_at').reverse()
+        recipies = []
+        for recipe in queryset:
+            total = int(recipe.duration.total_seconds())
+            hour = total // 60 // 60
+            minute = total // 60 % 60
+            second = total % 60
+            if hour > 0:
+                duration = "{} Hour {} Minutes".format(hour, minute)
+            else:
+                duration = "{} Minute {} Seconds".format(minute, second)
+            recipies.append({'id':recipe.id, 'name':recipe.name.upper(), 'rating':recipe.rating, 'duration':duration})
+
+        return JsonResponse({'data':recipies}, safe=False)
+
+    else:
+        return JsonResponse({'data':[]}, safe=False)
