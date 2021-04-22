b1aa01d1c260635bc0e145945c241da1d35b2357
Neel Patel <neelpatel1905.np@gmail.com>
2021-04-22 13:11:21 +0530

    expense db created

--- eatup_api/settings.py
+++ eatup_api/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'api',
     'web',
     'user',
+    'expense',
 ]
 
 MIDDLEWARE = [
---
+++ expense/models.py
@@ -0,0 +1,25 @@
+from django.db.models import *
+from recipe.models import *
+from user.models import *
+
+class Expense(Model):
+    schdule = ForeignKey(RecipeSchedule, on_delete=RESTRICT)
+    amount = DecimalField(max_digits=6, decimal_places=2, null=False)
+    user = ForeignKey(User, on_delete=RESTRICT)
+    created_at = DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "expense"
+
+    def __str__(self):
+        return "{} - {}".format(self.schdule.date, self.schdule.recipe.name)
+
+class ExpenseUser(Model):
+    expense = ForeignKey(Expense, on_delete=CASCADE)
+    user = ForeignKey(User, on_delete=RESTRICT)
+
+    class Meta:
+        db_table = "expense_user"
+
+    def __str__(self):
+        return "{} - {} - {}".format(self.expense.schdule.date, self.expense.schdule.recipe.name, self.user.name)
